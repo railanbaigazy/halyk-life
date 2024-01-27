f40b916766cc7df9da79c741cbada604e618b515
Railan Baigazy <69196082+railanbaigazy@users.noreply.github.com>
2024-01-28 05:58:56 +0600

    push

--- components/med/UploadForm.jsx
+++ components/med/UploadForm.jsx
@@ -1,33 +1,43 @@
 import DragAndDropFileUpload from "@components/med/DragDrop";
 
-const UploadForm = () => {
+const UploadForm = ({ submitting }) => {
     return (
         <form
             name="upload-form"
-            className="flex"
+            className="flex w-full gap-[20px]"
         >
-            <div className="flex flex-col w-1/2 bg-white p-[40px]">
-                <label>
+            <div className="flex flex-col flex-1 bg-white h-[480px] p-[40px] rounded-[10px]">
+                <label className="flex-1">
                     <span>ИИН</span>
                     <textarea className="form_input" required />
                 </label>
-                <label>
+                <label className="flex-1">
                     <span>ФИО</span>
-                    <textarea disabled />
+                    <textarea className="form_input" disabled />
                 </label>
-                <label>
+                <label className="flex-1">
                     <span>Дата рождения</span>
-                    <textarea disabled />
+                    <textarea className="form_input" disabled />
                 </label>
-                <div>
-                    <div>Мужчина</div>
-                    <div>Женщина</div>
-                </div>
+                <label className="flex-col">
+                    <span>Пол</span>
+                    <div className="flex-1 flex gap-[20px] mt-2.5">
+                        <div className="flex-1 std_button green_button">Мужчина</div>
+                        <div className="flex-1 std_button">Женщина</div>
+                    </div>
+                </label>
 
             </div>
-            <div className="flex flex-col w-1/2 bg-white">
-                <span>Прикрепление данных пациента</span>
+            <div className="flex flex-col flex-1 bg-white rounded-[10px] p-[40px]">
+                <span className="mb-1.5">Прикрепление данных пациента</span>
                 <DragAndDropFileUpload />
+                <button
+                    type="submit"
+                    disabled={submitting}
+                    className="std_button green_button"
+                >
+                    {submitting ? "Отправляется..." : "Отправить"}
+                </button>
             </div>
         </form>
     );

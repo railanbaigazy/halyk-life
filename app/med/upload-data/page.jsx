6a18e6e6da53475f7d1567d1528eb9fd093244f9
Railan Baigazy <69196082+railanbaigazy@users.noreply.github.com>
2024-01-28 04:01:27 +0600

    new updates

--- app/med/upload-data/page.jsx
+++ app/med/upload-data/page.jsx
@@ -2,8 +2,19 @@ import MainWrapper from "@components/MainWrapper";
 
 const MedUploadData = () => {
     return (
-        <MainWrapper>
-            MedUploadData
+        <MainWrapper className="flex gap-[20px]">
+            <form
+                name="upload-form"
+
+            >
+
+            </form>
+            <div className="flex flex-col w-1/2 bg-white">
+
+            </div>
+            <div className="flex flex-col w-1/2 bg-white">
+
+            </div>
         </MainWrapper>
     );
 };

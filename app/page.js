a28cf4d489a7fedd20e206f537f3a96639cb6f7c
Railan Baigazy <69196082+railanbaigazy@users.noreply.github.com>
2024-01-28 13:29:31 +0600

    update

--- app/page.js
+++ app/page.js
@@ -1,16 +1,4 @@
-'use client'
-
-import {useRouter} from "next/navigation"
-
 export default function Home() {
-    const isMed = true
-    const router = useRouter()
-
-    if (isMed) {
-        router.push("/med")
-    } else {
-        router.push("/admin")
-    }
   return (
     <div className="flex min-h-screen flex-col items-center justify-between p-24">
       <h1>HALYK</h1>

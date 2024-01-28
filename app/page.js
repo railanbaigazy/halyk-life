7b103603f180a0d830b9b79e99af8059a00bdc05
Railan Baigazy <69196082+railanbaigazy@users.noreply.github.com>
2024-01-28 13:21:35 +0600

    update

--- app/page.js
+++ app/page.js
@@ -1,6 +1,16 @@
-import Image from "next/image";
+'use client'
+
+import {useRouter} from "next/navigation"
 
 export default function Home() {
+    const isMed = true
+    const router = useRouter()
+
+    if (isMed) {
+        router.push("/med")
+    } else {
+        router.push("/admin")
+    }
   return (
     <div className="flex min-h-screen flex-col items-center justify-between p-24">
       <h1>HALYK</h1>
